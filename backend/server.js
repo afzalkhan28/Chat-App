03015f3859a82e075f9b3d292227755803564a0d
afzalkhan28 <139621886+afzalkhan28@users.noreply.github.com>
2024-10-20 18:40:58 +0530

    get message

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import messageRoutes from "./routes/message.routes.js"
 import authRoutes from "./routes/auth.routes.js"
 
 const app = express();
+
 const PORT = process.env.PORT || 7001;
 
 dotenv.config();
 app.use(express.json());
+app.use(cookieParser());
 
 
 
@@ -17,6 +21,7 @@ app.use(express.json());
 // });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(7001, () => {
     connectToMongoDB();
